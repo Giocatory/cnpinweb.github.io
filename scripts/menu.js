f8da3474053ae60439888391d8feb75b2326a292
Giocatory <nivolyas@gmail.com>
2021-12-22 16:49:56 +0300

    main

--- scripts/menu.js
+++ scripts/menu.js
@@ -8,7 +8,7 @@ if (animItems.length > 0) {
             const animItem = animItems[index];
             const animItemHeight = animItem.offsetHeight;
             const animItemOffset = offset(animItem).top;
-            const animStart = 4;
+            const animStart = 5;
 
             let animItemPoint = window.innerHeight - animItemHeight / animStart;
 
